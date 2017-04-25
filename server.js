8ab2e0642e7153b53c6fe39eb5f880d395a47e6f
galaxyaurona <galaxyaurona@gmail.com>
2017-04-25 12:40:34 +1000

    second commit with proper server and log

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require('express');
 var path = require('path');
 var multer = require('multer')
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 var sys = require('sys')
+const morgan = require('morgan');
 var exec = require('child_process').exec;
-
-
+var fs = require('fs')
+var util = require('util');
+// 
+var log_stdout = process.stdout;
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -25,6 +30,11 @@ var upload = multer(
         preservePath: true
     }
 )
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+app.use(morgan('combined',{stream: accessLogStream}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/upload', upload.single('image'), function (req, res) {
     if (!req.file)
@@ -35,7 +45,7 @@ app.post('/upload', upload.single('image'), function (req, res) {
     var full_path = path.join(__dirname, image.path);
     var docker_command = "docker exec fox-recognition python label_image.py /classifying_images/" + image.filename;
     exec(docker_command, function (error, stdout, stderr) {
-        sys.print('stdout: ' + stdout);
+     
         if (error !== null) {
             console.log('exec error: ' + error);
         }
@@ -43,7 +53,7 @@ app.post('/upload', upload.single('image'), function (req, res) {
         json_response = {};
         Object.keys(docker_response).forEach(function(key,index,array){
             docker_response[key] = parseFloat(docker_response[key])
-            json_response[key] = parseFloat(docker_response[key]) *100+ " %"
+            json_response[key] = Math.round(parseFloat(docker_response[key]) *100)+ " %"
         })
         json_response["prediction"] = docker_response["not fox"] > docker_response["fox"] ? "Image does not containt a fox" : "Image contains a fox" 
         res.json(json_response);
@@ -53,8 +63,23 @@ app.post('/upload', upload.single('image'), function (req, res) {
 
 
 });
+console.log = function(d) { //
+  accessLogStream.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+// static route for sending data
+app.post('/measurement/measurements', function(req, res) {
+     console.log("Body of request: "+JSON.stringify(req.body))
+    res.json({"success":"true"});
+});
+app.get('/log', function(req, res) {
+  
+    res.sendFile(path.join(__dirname, 'access.log'));
+});
+// root index
 app.use('/', express.static(path.join(__dirname, 'public')))
-let running_port = process.env.PORT |3000;
+
+let running_port = process.env.PORT | 3000;
 app.listen(running_port, function () {
     console.log('Example app listening on port ' + running_port)
 })
